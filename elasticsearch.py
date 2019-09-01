0247a66f2a694860ce971a3ea4d4525d53bdd250
Rob Orndoff <45725975+orndor@users.noreply.github.com>
2019-09-01 19:01:41 -0400

    Improvements on how this code obtained the public IP.

--- elasticsearch.py
+++ elasticsearch.py
@@ -2,8 +2,8 @@
     File name: es-access-policy-update.py
     Author: Robo
     Date created: 01/03/2019
-    Date last modified: 01/09/2019
-    Python Version: 3.6
+    Date last modified: 09/01/2019
+    Python Version: 3.7
 
 Background: A logstash golden image EC2 spot instance may spin up
 spontaniously, and the Elasticsearch server it pipelines logs to needs
@@ -16,40 +16,21 @@ document. The policy document is then applied to an AWS Elasticsearch domain
 to allow the originally matched EC2 instance access.
 '''
 # Import the AWS SDK
-import boto3
+from boto3 import client, resource
 
-
-# Required function definition for AWS
 def lambda_handler(event, context):
     # Predefine some variables so we can easily update input parameters.
-    key = 'Name'
-    value = 'logstash'
     es_domain = 'examplees'
     es_arn = "PASTE FULL ESDOMAIN ARN BETWEEN QUOTES"
-    # Create a boto3 EC2 client
-    ec2client = boto3.client('ec2')
+    # Create a boto3 EC2 resource
+    ec2 = resource('ec2')
     # Create a boto3 Elasticsearch client
-    esclient = boto3.client('es')
-    # Query AWS for an EC2 instance which matches the predefined tag
-    response = ec2client.describe_instances(
-        Filters=[
-            {
-                'Name': 'tag:'+key,
-                'Values': [value]
-            }
-        ]
-    )
-    if response["Reservations"] != []:
-        # Find the public IP address of the instance within the response.
-        public_ip = response\
-        ["Reservations"][0]\
-        ["Instances"][0]\
-        ["NetworkInterfaces"][0]\
-        ["PrivateIpAddresses"][0]\
-        ["Association"]\
-        ["PublicIp"]
-        # Craft a new Policy Document with the new public IP address,
-        # and push it to a specific elastic search instance.
+    esclient = client('es')
+    # Use the inbound event data to grab the insance-id; then use that to get the public IP.
+    instance = ec2.Instance(event['instance-id'])
+    public_ip = instance.public_ip_address
+
+    # Insert the public IP address of the instance into the ES access policy.
         esclient.update_elasticsearch_domain_config(
             DomainName=es_domain, AccessPolicies='{"Version":"2012-10-17",'
             '"Statement":[{"Effect":"Allow","Principal":{"AWS":"*"},'
@@ -57,7 +38,3 @@ def lambda_handler(event, context):
             '"Condition":{"IpAddress":{"aws:SourceIp":'
             '["' + public_ip + '"]}}}]}'
         )
-    else:
-        pass
-
-    return
