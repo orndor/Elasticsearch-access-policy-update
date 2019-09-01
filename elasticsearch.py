ecafdccbd03114766be1cf3f9d130cce958836cd
Rob Orndoff <45725975+orndor@users.noreply.github.com>
2019-09-01 19:07:29 -0400

    Update elasticsearch.py

--- elasticsearch.py
+++ elasticsearch.py
@@ -4,16 +4,6 @@
     Date created: 01/03/2019
     Date last modified: 09/01/2019
     Python Version: 3.7
-
-Background: A logstash golden image EC2 spot instance may spin up
-spontaniously, and the Elasticsearch server it pipelines logs to needs
-to allow it (with a new public IP) to connect.
-
-This function is intended to be used in AWS Lambda to find a specific EC2
-spot instance with a specific tag. If this script finds a match, it pulls
-the public IP address of that instances and then creates a JSON policy
-document. The policy document is then applied to an AWS Elasticsearch domain
-to allow the originally matched EC2 instance access.
 '''
 # Import the AWS SDK
 from boto3 import client, resource
